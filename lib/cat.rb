df59e8d80f53ff892cd5893868ae75d7dfa8d872
Sharon Watkins <swatkins876@gmail.com>
2019-10-09 17:42:56 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,7 +3,7 @@ class Cat
   attr_reader :name 
   attr_accessor :owner, :mood 
   
-  def initialize(name, owner)
+  def initialize(name, owner, mood=nervous)
     @name = name 
     @owner = owner 
     @mood = mood 
