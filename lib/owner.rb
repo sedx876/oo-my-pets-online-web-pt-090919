1c8c4327df3ccc2b05e460a42bf3354ea4680a86
Sharon Watkins <swatkins876@gmail.com>
2019-10-09 17:26:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,8 +2,9 @@ class Owner
   
   attr_reader :name 
   
-  def initialize(name)
-    @name = name 
+  def initialize(name, species = human)
+    @name = name
+    @species = species 
   end 
   
 end
